4a531d37ab9081516ee630402c5aff0d2c5c7053
EC2 Default User <ec2-user@ip-172-31-88-92.ec2.internal>
2022-10-08 10:45:45 +0000

    BooksController作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
+  get 'books/index'
+  get 'books/show'
+  get 'books/edit'
   devise_for :users
   root to: 'homes#top'
   get '/home/about', to: 'homes#about'
   resources :users, only: [:index, :show, :edit]
+  resources :books, only: [:index, :show, :edit]
 end
