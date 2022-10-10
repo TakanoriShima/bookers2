04d07f8ce007c809e8965d1128f93d6c705e0832
EC2 Default User <ec2-user@ip-172-31-88-92.ec2.internal>
2022-10-10 08:52:04 +0000

    RSpec pass

--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,6 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
   attachment :profile_image       
   has_many :books, dependent: :destroy
-  validates :name, presence: true, length: { minimum: 2, maximum: 20 }, uniqueness: true
+  validates :name, length: { minimum: 2, maximum: 20 }, uniqueness: true
   validates :introduction, presence: false, length: { maximum: 50 }
 end
--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = '20e4940a813c7c0a954339c1fe03ac8dc0156a8cdff6cae025174da7abdc6cff9b4a310c5ca96e3f16fb1b94a0d46f25e3bc8cb9dc01e7067cd6b2c7dc7e5e52'
